da46ae683721bcbb19b563adb674ccf755f8357c
hadian <hadian.online@gmail.com>
2020-07-14 20:36:49 +0700

    home - enable Base Layout for HomePage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tasikcode_app_flutter/utils/my_app.dart';
 import 'package:tasikcode_app_flutter/utils/my_colors.dart';
@@ -10,19 +11,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  List<Widget> _widgetOptions = <Widget>[
+    Container(child: Center(child: Text('Index 0: Home'))),
+    Container(child: Center(child: Text('Index 1: Blog'))),
+    Container(child: Center(child: Text('Index 2: Event'))),
+    Container(child: Center(child: Text('Index 3: Baraya'))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(context),
-      body: Container(
-        child: Center(
-            child: Text(
-          "Blank App",
-          style: TextStyle(
-            color: MyColors.bluePrimary,
-          ),
-        )),
-      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: _bottomNavigationBar(context),
     );
   }
 
@@ -53,6 +56,51 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            FontAwesome.bell_o,
+            color: MyColors.bluePrimary,
+          ),
+          onPressed: () => null,
+        )
+      ],
     );
+  }
+
+  _bottomNavigationBar(BuildContext context) {
+    TextStyle _labelStyle = GoogleFonts.montserrat();
+
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: MyColors.bluePrimary,
+      unselectedItemColor: MyColors.grey,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesome.home),
+          title: Text("Home", style: _labelStyle),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesome.newspaper_o),
+          title: Text("Blog", style: _labelStyle),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesome.calendar_o),
+          title: Text("Event", style: _labelStyle),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(FontAwesome.users),
+          title: Text("Baraya", style: _labelStyle),
+        ),
+      ],
+    );
+  }
+
+  _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
--- lib/utils/my_colors.dart
+++ lib/utils/my_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 mixin MyColors {
   static const bluePrimary = Color(0xFF162447);
   static const yellowSecond = Color(0xFFFFBF44);
+  static const grey = Color(0xFFCACDD3);
 }
