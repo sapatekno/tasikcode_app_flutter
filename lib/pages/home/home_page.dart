bbc41e929bc2182efffc694e293d92fcf689cc92
hadian <hadian.online@gmail.com>
2020-07-14 16:27:28 +0700

    home - add Google Font Library

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tasikcode_app_flutter/utils/my_colors.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,11 +11,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
-      ),
+      appBar: _appBar(context),
       body: Container(
-        child: Center(child: Text("Blank App")),
+        child: Center(
+            child: Text(
+          "Blank App",
+          style: TextStyle(
+            color: MyColors.bluePrimary,
+          ),
+        )),
+      ),
+    );
+  }
+
+  _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: Row(
+        children: <Widget>[
+          Text(
+            "Tasik",
+            style: GoogleFonts.rubik(
+              fontWeight: FontWeight.w600,
+              color: MyColors.bluePrimary,
+            ),
+          ),
+          Text(
+            "code",
+            style: GoogleFonts.rubik(
+              fontWeight: FontWeight.w600,
+              color: MyColors.yellowSecond,
+            ),
+          ),
+        ],
       ),
     );
   }
