e7c67acb659fbc2385f52cd2cff379c4f0d13f45
hadian <hadian.online@gmail.com>
2020-10-02 08:37:18 +0700

    main - add global font theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tasikcode_app_flutter/pages/home/home_page.dart';
 
 void main() => runApp(new MyApp());
@@ -12,7 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'TasikCode App',
-      theme: new ThemeData(primarySwatch: Colors.blue),
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       routes: routes,
     );
   }
